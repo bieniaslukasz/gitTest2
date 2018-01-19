d3c755bb9c5834d3157df784bafd9115f7cc4379
bieniaslukasz <s162377@student.dtu.dk>
2018-01-19 16:44:12 +0100

    Initial commit - test

---
+++ CoreJavaDemos/src/com/srccodes/example/CoreJavaDemos.java
@@ -0,0 +1,14 @@
+package com.srccodes.example;
+
+public class CoreJavaDemos {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("git test success");
+
+	}
+
+}
